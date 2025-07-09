452378d46c8e47644b6049df1062fe68a623fb98
German Eduardo Salazar Aranda <german2506.gs@gmail.com>
2025-07-08 18:20:05 -0700

    capitalizing alumn names on table

--- src/components/dashboard/alumns/table.tsx
+++ src/components/dashboard/alumns/table.tsx
@@ -147,7 +147,7 @@ export default function AlumnsTable() {
         <tbody className={isLoading ? "opacity-50 pointer-events-none" : ""}>
           {alumns.map((alumn) =>
             <tr key={`alumn_${alumn.id}`} onClick={() => navigate(`/alumns/form/${alumn.id}`)} className={"odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600 even:dark:hover:bg-gray-700 capitalize"}>
-              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white capitalize">
                 {alumn.name}
               </th>
               <td className="px-6 py-4 capitalize">
