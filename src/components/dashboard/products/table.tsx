743fa2481be24b0b7c9df57de375f0a6daab3e60
German Eduardo Salazar Aranda <german2506.gs@gmail.com>
2025-07-20 14:45:40 -0700

    added search function by name in products

--- src/components/dashboard/products/table.tsx
+++ src/components/dashboard/products/table.tsx
@@ -13,9 +13,19 @@ export default function ProductsTable() {
   const [links, setLinks] = useState<ILinks>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [errors, setErrors] = useState<{ msj: string }[]>([]);
+  const [searchValue, setSearchValue] = useState('');
+
   let [searchParams] = useSearchParams();
 
   useEffect(() => {
+    if (searchParams.has('page[page]')) {
+      setCurrentPage(parseInt(searchParams.get('page[page]')!))
+    } else {
+      setCurrentPage(1)
+    }
+    if (searchParams.has('q[name_cont]')) {
+      setSearchValue(searchParams.get('q[name_cont]')!)
+    }
     loadProducts();
   }, [searchParams.toString()])
 
@@ -38,10 +48,50 @@ export default function ProductsTable() {
     })
   }
 
+  function setPage(page: number) {
+    const newParams = new URLSearchParams(searchParams)
+    newParams.set('page[page]', page.toString());
+    navigate(`?${newParams.toString()}`, { replace: true });
+  }
+
+  function resetPager() {
+    if (currentPage === 1) {
+      loadProducts();
+    } else {
+      setPage(1)
+    }
+  }
+
+  function handleKeyDown(e: React.KeyboardEvent) {
+    if (e.key === 'Enter') {
+      const newParams = new URLSearchParams(searchParams);
+      newParams.delete('page[page]')
+      if (searchValue.trim()) {
+        newParams.set('q[name_cont]', searchValue.trim());
+      } else {
+        newParams.delete('q[name_cont]');
+      }
+      navigate(`?${newParams.toString()}`, { replace: true });
+    }
+  };
+
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg my-10 mx-6">
       <div className="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
         <div>
+          <label htmlFor="table-search" className="sr-only">Search</label>
+          <div className="relative mt-1">
+            <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+              </svg>
+            </div>
+            <input type="text"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+              onKeyDown={handleKeyDown}
+              id="table-search" className="block pt-2 ps-10 pb-2 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for items" />
+          </div>
         </div>
         <div className="relative">
           <button id="dropdownRadioButton" onClick={() => navigate('/products/form')} className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
