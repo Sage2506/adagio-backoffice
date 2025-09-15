57cdb1b85688a404dd19c23a18276ba145a1dcd8
German Eduardo Salazar Aranda <german2506.gs@gmail.com>
2025-09-14 18:55:36 -0700

    changed w3icons to heroicons

--- src/components/dashboard/alumns/row.tsx
+++ src/components/dashboard/alumns/row.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import type { IAlumnRecord } from "../../../types/alumns";
 import { useNavigate } from "react-router";
+import { TrashIcon } from "@heroicons/react/24/solid";
 
 interface IAlumnsRow {
   alumn: IAlumnRecord
@@ -24,7 +25,12 @@ export default function AlumnsRow({ alumn, handleDelete }: IAlumnsRow) {
         {alumn.last_name}
       </td>
       <td className="px-6 py-4">
-        <button onClick={ e => handleDelete(e, alumn)}>Delete</button>
+        <button
+          onClick={e => handleDelete(e, alumn)}
+          className="p-2 text-red-600 hover:text-red-800 hover:bg-red-100 dark:hover:text-red-400 dark:hover:bg-red-900 rounded-full transition-colors duration-200"
+        >
+          <TrashIcon className="w-5 h-5" />
+        </button>
       </td>
     </tr>
   );
